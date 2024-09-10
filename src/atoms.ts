6c815d8178f92473ee9c31f6983f1ea922031d51
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-09-10 12:21:08 +0900

    init

---
+++ src/atoms.ts
@@ -0,0 +1,35 @@
+import { atom, selector } from "recoil";
+
+export enum Categories {
+  "TO_DO" = "TO_DO",
+  "DOING" = "DOING",
+  "DONE" = "DONE",
+}
+
+export interface IToDo {
+  text: string;
+  id: number;
+  category: Categories;
+}
+
+export const categoryState = atom<Categories>({
+  key: "category",
+  default: Categories.TO_DO,
+});
+
+let output = localStorage.getItem("toDos") || "[]";
+let localData = JSON.parse(output as any);
+
+export const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: localData,
+});
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
+  },
+});
